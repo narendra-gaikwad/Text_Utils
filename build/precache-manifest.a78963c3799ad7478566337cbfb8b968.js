22c3dffd9d2cea94dc8bdf3ae0c5178dd775cc24
Narendra Gaikwad <94712968+narendra-gaikwad@users.noreply.github.com>
2022-09-26 13:28:43 +0530

    Text_Utils

---
+++ build/precache-manifest.a78963c3799ad7478566337cbfb8b968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a739d0c373c03cd6def",
+    "url": "/static/css/main.3b2e1a8f.chunk.css"
+  },
+  {
+    "revision": "4a739d0c373c03cd6def",
+    "url": "/static/js/main.4a739d0c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "e54c96d7b20cf872c4890ef8ba2cd84b",
+    "url": "/index.html"
+  }
+];
